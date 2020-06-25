474ca63de617e6b3f84a3661ee58b96d32683c85
Andriy <refwol131@mail.ru>
2020-06-26 01:13:05 +0300

    Updates

---
+++ precache-manifest.8aec899ff70415e7e933c11e19c8db49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10dba9cad65ad77dd6cd19ca9ec9282",
+    "url": "/test2(test)/index.html"
+  },
+  {
+    "revision": "449f4f21084e9e48c339",
+    "url": "/test2(test)/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd3e159ce3ddf4750c33",
+    "url": "/test2(test)/static/js/2.0a9c3f72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test2(test)/static/js/2.0a9c3f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449f4f21084e9e48c339",
+    "url": "/test2(test)/static/js/main.cee63fea.chunk.js"
+  },
+  {
+    "revision": "e6e16be3f193f27b7b29",
+    "url": "/test2(test)/static/js/runtime-main.cc0273f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test2(test)/static/media/logo.5d5d9eef.svg"
+  }
+]);
