ebf8e4ab8e622ef3beba2badcd241f29c7cdef58
Xiaomi <andriiyarotskiy@gmail.com>
2020-11-08 22:52:17 +0200

    clear app

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Say hello Ignat
-        </a>
-      </header>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-        "q"
+
     </div>
   );
 }
