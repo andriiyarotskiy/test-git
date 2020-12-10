ca895cd0b2552eabbb0eb62da85aedfb84627a68
Xiaomi <andriiyarotskiy@gmail.com>
2020-12-10 11:22:13 +0200

    custom hook input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import Main from "./Main";
 import Alert from "./alert/Alert";
 import AlertProvider from "./alert/AlertContext";
 
 
+const useLogger = (value) => {
+    useEffect(() => {
+        console.log('Value changed', value)
+    }, [value])
+}
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue)
+
+    const onChange = (e) => {
+        setValue(e.currentTarget.value)
+    }
+    const clear = () => {
+        setValue('')
+    }
+    return {
+        bind: {value, onChange},
+        value,
+        clear
+    }
+}
+
 function App() {
+
+    const input = useInput('')
+    useLogger(input.value)
 
     return (
         <>
@@ -16,8 +40,10 @@ function App() {
                 </div>
             </AlertProvider>*/}
             <div className="container pt-3">
-
-                <input type="text"/>
+                <input type="text" {...input.bind}/>
+                <button className="btn btn-warning" onClick={() => input.clear()}>Очистить</button>
+                <hr/>
+                <h1>{input.value}</h1>
             </div>
         </>
 
