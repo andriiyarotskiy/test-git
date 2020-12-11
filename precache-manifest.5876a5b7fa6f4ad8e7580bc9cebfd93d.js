40d5f548814f6bfe58b4857460f8ee35c54e59b8
Xiaomi <andriiyarotskiy@gmail.com>
2020-12-11 13:27:20 +0200

    Updates

--- precache-manifest.5876a5b7fa6f4ad8e7580bc9cebfd93d.js
+++ precache-manifest.5876a5b7fa6f4ad8e7580bc9cebfd93d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9c19b54b4a3135c3f3d462689cc821f",
+    "revision": "9a1e2721e418b8e6594b4c038834ddfb",
     "url": "/test-git/index.html"
   },
   {
-    "revision": "2199bb8ff885b69861a0",
+    "revision": "4e22f86900b8b9da8f5a",
     "url": "/test-git/static/css/2.3cf5d70f.chunk.css"
   },
   {
-    "revision": "2199bb8ff885b69861a0",
-    "url": "/test-git/static/js/2.5e0de0b0.chunk.js"
+    "revision": "4e22f86900b8b9da8f5a",
+    "url": "/test-git/static/js/2.757c91c4.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/test-git/static/js/2.5e0de0b0.chunk.js.LICENSE.txt"
+    "url": "/test-git/static/js/2.757c91c4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d7b3c155230b126e7571",
-    "url": "/test-git/static/js/main.b68bcf4f.chunk.js"
+    "revision": "8cb41f1f211998f174ea",
+    "url": "/test-git/static/js/main.e96986f8.chunk.js"
   },
   {
     "revision": "9ac19fc8f4ca0d613d4c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-git/precache-manifest.c9bba7f16cdce8d3fe74d68d2708f8e7.js"
+  "/test-git/precache-manifest.5876a5b7fa6f4ad8e7580bc9cebfd93d.js"
 );
 
 self.addEventListener('message', (event) => {
