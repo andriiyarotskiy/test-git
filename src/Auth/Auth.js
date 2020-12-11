22f40333024d3027a5b856830c36ea33e8ea3033
Xiaomi <andriiyarotskiy@gmail.com>
2020-12-11 13:26:38 +0200

    fb auth

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,20 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 
-const Auth = () => {
-
+const Auth = ({children}) => {
         const [state, setIsAuth] = useState({
-            isAuth: false,
             name: '',
-            accessToken: ''
+            accessToken: '',
+            img: null
         })
 
-        useEffect(() => {
-
-        }, [])
-
         const responseGoogle = (response) => {
-            if(response){
+            if (response) {
                 setIsAuth({
                     ...state,
                     name: response.profileObj.name,
@@ -30,27 +26,53 @@ const Auth = () => {
             alert('Failed to log out')
         }
 
+        //FACEBOOK
+        const responseFacebook = (response) => {
+            console.log(response)
+            setIsAuth({
+                ...state,
+                name: response.name,
+                isAuth: true,
+                img: response.picture.data.url
+            })
+        }
+
 
         return (
             <div style={{textAlign: "center"}}>
-                {!state.isAuth ?
-                    <GoogleLogin clientId="345999559557-l0djs7ot59j8914n1bp4mvkn08odsp4n.apps.googleusercontent.com"
+                {!state.isAuth
+                    ? <div><GoogleLogin clientId="345999559557-l0djs7ot59j8914n1bp4mvkn08odsp4n.apps.googleusercontent.com"
                         /*       buttonText="Login with Google"*/
-                                 onSuccess={responseGoogle}
-                                 onFailure={responseGoogle}
-                                 cookiePolicy={'single_host_origin'}
-                                 responseType='code,token'
-                    />
+                                        onSuccess={responseGoogle}
+                                        onFailure={responseGoogle}
+                                        cookiePolicy={'single_host_origin'}
+                                        responseType='code,token'
+                                        isSignedIn={true}
+                                        render={renderProps =>
+                                            <button onClick={renderProps.onClick} className="btn btn-dark">Sign in</button>
+                                        }/>
+                        <div>
+                            <FacebookLogin
+                                appId="399813427801800"
+                                autoLoad={false}
+                                fields="name,email,picture"
+                                callback={responseFacebook}/>
+                        </div>
+
+                    </div>
+
                     : <div>
                         <h1>{state.name ? state.name : null}</h1>
+                        <img src={state.img} alt="ava"/>
+                        <span>{children}</span>
                         <GoogleLogout
                             clientId={"345999559557-l0djs7ot59j8914n1bp4mvkn08odsp4n.apps.googleusercontent.com"}
                             buttonText={'Logout'}
                             onLogoutSuccess={logout}
                             onFailure={handleLogoutFailure}
-                        ></GoogleLogout>
+                        />
                     </div>}
-                {state.accessToken && <h5> token : {state.accessToken}</h5>}
+                {state.accessToken && <span> token : {state.accessToken}</span>}
             </div>
         );
     }
